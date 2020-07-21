d585fbb5fef4adc738902bd128598398d2f6a0c6
xenomech <gokulsureshkumarak@gmail.com>
2020-07-21 22:53:48 +0530

    😂 Removed the animation code from stack overflow ! not working as expected!😂

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -16,123 +16,127 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 100.0, left: 10.0),
-              child: Row(
-                children: <Widget>[
-                  FlatButton.icon(
-                    icon: Icon(Icons.arrow_back),
-                    label: Text(""),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  Text(
-                    "Hey There !",
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 15, left: 20, right: 20),
-              child: Container(
-                width: 300.0,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 50),
-                    Text(
-                      "Enter Email an Password to Register",
-                      style: TextStyle(
-                          fontSize: 15.0, fontStyle: FontStyle.italic),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() => email = value);
-                      },
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() => password = value);
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    InkWell(
-                      borderRadius: BorderRadius.circular(30.0),
-                      onTap: () {},
-                      child: Ink(
-                          height: 50,
-                          width: 250,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            // color: Colors.white,
-                            border: Border.all(color: Colors.grey),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Register",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.green),
-                            ),
-                          )),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "already part of the team?",
-                      style: TextStyle(
-                          fontSize: 15.0, fontStyle: FontStyle.italic),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    InkWell(
-                        borderRadius: BorderRadius.circular(30.0),
-                        onTap: () {
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 100.0, left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      FlatButton.icon(
+                        icon: Icon(Icons.arrow_back),
+                        label: Text(""),
+                        onPressed: () {
                           Navigator.pop(context);
                         },
-                        child: Ink(
-                          height: 50,
-                          width: 250,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            color: Colors.green,
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
+                      ),
+                      Text(
+                        "Hey There !",
+                        style: TextStyle(
+                            fontSize: 30.0, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 15, left: 20, right: 20),
+                  child: Container(
+                    width: 300.0,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 50),
+                        Text(
+                          "Enter Email an Password to Register",
+                          style: TextStyle(
+                              fontSize: 15.0, fontStyle: FontStyle.italic),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          onChanged: (value) {
+                            setState(() => email = value);
+                          },
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              fontSize: 20.0,
                             ),
                           ),
-                        )),
-                  ],
-                ),
-              ),
-            )
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          onChanged: (value) {
+                            setState(() => password = value);
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30.0),
+                        InkWell(
+                          borderRadius: BorderRadius.circular(30.0),
+                          onTap: () {},
+                          child: Ink(
+                              height: 50,
+                              width: 250,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                // color: Colors.white,
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Register",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.green),
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "already part of the team?",
+                          style: TextStyle(
+                              fontSize: 15.0, fontStyle: FontStyle.italic),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        InkWell(
+                            borderRadius: BorderRadius.circular(30.0),
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Ink(
+                              height: 50,
+                              width: 250,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                color: Colors.green,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.white),
+                                ),
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
--- lib/Screens/Authenticate/Sign_in.dart
+++ lib/Screens/Authenticate/Sign_in.dart
@@ -21,7 +21,7 @@ class _SigninState extends State<Signin> {
         child: Column(
           children: <Widget>[
             SizedBox(
-              height: 30.0,
+              height: 5.0,
             ),
             Container(
               padding: EdgeInsets.only(left: 30.0, top: 150.0),
@@ -51,7 +51,7 @@ class _SigninState extends State<Signin> {
                 width: 300.0,
                 child: Column(
                   children: <Widget>[
-                    SizedBox(height: 50),
+                    SizedBox(height: 40),
                     Text(
                       "Enter Email and Password to Login",
                       style: TextStyle(
@@ -60,30 +60,47 @@ class _SigninState extends State<Signin> {
                     SizedBox(
                       height: 20,
                     ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() => email = value);
-                      },
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          fontSize: 20.0,
-                        ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 20.0,
+                              // spreadRadius: 3.0,
+                              color: Colors.grey.shade400)
+                        ],
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() => password = value);
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          fontSize: 20.0,
-                        ),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            onChanged: (value) {
+                              setState(() => email = value);
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextField(
+                            onChanged: (value) {
+                              setState(() => password = value);
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                     SizedBox(height: 30.0),
@@ -94,6 +111,12 @@ class _SigninState extends State<Signin> {
                           height: 50,
                           width: 250,
                           decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 10.0,
+                                  // spreadRadius: 3.0,
+                                  color: Colors.grey.shade400)
+                            ],
                             borderRadius: BorderRadius.circular(30.0),
                             color: Colors.green,
                           ),
@@ -118,50 +141,53 @@ class _SigninState extends State<Signin> {
                     ),
                     InkWell(
                       borderRadius: BorderRadius.circular(30.0),
-                      onTap: () {
-                        Navigator.of(context).push(_createRoute());
-                      },
+                      onTap: () {},
                       child: Ink(
                           height: 50,
                           width: 250,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 10.0,
+                                  // spreadRadius: 3.0,
+                                  color: Colors.grey.shade400)
+                            ],
                             // color: Colors.white,
-                            border: Border.all(color: Colors.grey),
                           ),
-                          child: Center(
-                            child: Text(
-                              "Register",
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.green),
+                          child: Container(
+                            height: 50,
+                            width: 250,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30.0),
+                              // color: Colors.white,
+                              border: Border.all(color: Colors.grey),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Register",
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.green),
+                              ),
                             ),
                           )),
-                    )
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  children: <Widget>[],
+                ))
           ],
         ),
       ),
     );
   }
-}
-
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => Register(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(0.0, 0.5);
-      var end = Offset.zero;
-      var curve = Curves.easeOut;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
 }
