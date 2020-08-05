c357faade8d7fe2f39c6509b5508998ea52fa834
xenomech <gokulsmenon227@gmail.com>
2020-08-05 15:52:05 +0530

    👀👀 trying to impliment a users page

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sreevaidyanatham/Services/auth.dart';
+// import 'package:sreevaidyanatham/Services/auth.dart';
+import 'package:sreevaidyanatham/Screens/UserScreen/userScreen.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -9,39 +10,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final AuthService _auth = AuthService();
+  // final AuthService _auth = AuthService();
   String _bookedtime = "hi there";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: Text("Hi there !"),
-          elevation: 0,
-          backgroundColor: Colors.white,
-          actions: <Widget>[
+        body: Column(
+      children: <Widget>[
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          children: <Widget>[
+            Text("Hi there !"),
             IconButton(
               icon: Icon(Icons.account_circle),
               onPressed: () {
-                _auth.signout();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UserScreen()),
+                );
               },
-            )
+            ),
           ],
         ),
-        body: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            Text(_bookedtime),
-            RaisedButton(
-              onPressed: () {
-                showTimePicker(context: context, initialTime: TimeOfDay.now())
-                    .then((value) =>
-                        {setState(() => _bookedtime = value.toString())});
-              },
-            )
-          ],
-        ));
+        RaisedButton(
+          onPressed: () {
+            showTimePicker(context: context, initialTime: TimeOfDay.now()).then(
+                (value) => {setState(() => _bookedtime = value.toString())});
+          },
+        )
+      ],
+    ));
   }
 }
 
---
+++ lib/Screens/UserScreen/userScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatefulWidget {
+  UserScreen({Key key}) : super(key: key);
+
+  @override
+  _UserScreenState createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
