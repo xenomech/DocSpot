148806e0f755621a23a1a70baf34d79aa4667a06
xenomech <gokulsmenon227@gmail.com>
2020-08-09 18:02:58 +0530

    👀 portrait lock and ui changes

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -16,9 +16,13 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
+    var height = MediaQuery.of(context).size.height;
+
     void _showapointmentpanel() {
       String _name;
-      DateTime _date;
+      String _date;
+      String _time;
+      String _doctor;
 
       showModalBottomSheet(
           elevation: 5,
@@ -29,15 +33,17 @@ class _HomeState extends State<Home> {
               child: StreamBuilder<Userdata>(
                   stream: DatabaseService(uid: user.uid).userdata,
                   builder: (context, snapshot) {
-                    return Form(
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(onChanged: (value) => _name = value),
-                          Text("$_name"),
-                          Text(_date.toString()),
-                        ],
-                      ),
-                    );
+                    if (snapshot.hasData) {
+                      return Form(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(onChanged: (value) => _name = value),
+                            Text("$_name"),
+                            Text(_date.toString()),
+                          ],
+                        ),
+                      );
+                    }
                   }),
             );
           });
@@ -78,58 +84,76 @@ class _HomeState extends State<Home> {
     return StreamProvider<Userdata>.value(
       value: DatabaseService(uid: user.uid).userdata,
       child: Scaffold(
+        backgroundColor: Colors.green,
         body: Column(
-          children: <Widget>[
+          children: [
             Container(
-              padding: EdgeInsets.only(left: 30.0, top: 80.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              height: height * .90,
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 20.0,
+                        // spreadRadius: 3.0,
+                        color: Colors.black)
+                  ],
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(50))),
+              child: Column(
                 children: <Widget>[
-                  Text(
-                    "Hi there !",
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    padding: EdgeInsets.only(left: 30.0, top: 80.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Hi there !",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () => _showuserspanel(),
+                          child: Center(
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 40,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  FlatButton(
-                    onPressed: () => _showuserspanel(),
-                    child: Center(
-                      child: Icon(
-                        Icons.account_circle,
-                        size: 40,
-                        color: Colors.green,
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 175.0),
+                        child: Text(
+                          "Your Bookings :",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
+                      Container(
+                        height: 400,
+                        child: Bookinglist(),
+                      ),
+                    ],
                   ),
+                  RaisedButton(
+                    child: Text("tryout"),
+                    onPressed: () => _showapointmentpanel(),
+                  )
                 ],
               ),
             ),
-            SizedBox(
-              height: 40,
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 175.0),
-                  child: Text(
-                    "Your Bookings :",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  height: 400,
-                  child: Bookinglist(),
-                ),
-              ],
-            ),
-            RaisedButton(
-              child: Text("tryout"),
-              onPressed: () => _showapointmentpanel(),
-            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sreevaidyanatham/Models/user.dart';
 import 'package:sreevaidyanatham/Screens/wrapper.dart';
@@ -9,6 +10,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
